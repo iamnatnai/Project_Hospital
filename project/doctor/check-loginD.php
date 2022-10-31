7d6155579ce1c48535255518f71e09a7ab9f1726
iamnatnai <92110407+iamnatnai@users.noreply.github.com>
2022-10-31 21:59:55 +0700

    Add files via upload

---
+++ project/doctor/check-loginD.php
@@ -0,0 +1,31 @@
+<?php
+    try {
+        $pdo = new PDO("mysql:host=localhost;dbname=miniproject;charset=utf8", "root", "");
+    } catch (PDOException $e) {
+        echo "เกิดข้อผิดพลาด : ".$e->getMessage();
+    }
+  
+  session_start();
+
+  $stmt = $pdo->prepare("SELECT * FROM doctor WHERE did = ? AND password = ?");
+  $stmt->bindParam(1, $_POST["username"]);
+  $stmt->bindParam(2, $_POST["password"]);
+  $stmt->execute();
+  $row = $stmt->fetch();
+
+  // หาก username และ password ตรงกัน จะมีข้อมูลในตัวแปร $row
+  if (!empty($row)) { 
+    // นำข้อมูลผู้ใช้จากฐานข้อมูลเขียนลง session 2 ค่า
+    $_SESSION["fullname"] = $row["dfnamelname"];   
+    $_SESSION["username"] = $row["did"];
+
+    // แสดง link เพื่อไปยังหน้าต่อไปหลังจากตรวจสอบสำเร็จแล้ว
+    echo "เข้าสู่ระบบสำเร็จ<br>";
+    echo "<a href='user-home.php'>ไปยังหน้าหลักของผู้ใช้</a>"; 
+
+  // กรณี username และ password ไม่ตรงกัน
+  } else {
+    echo "ไม่สำเร็จ ชื่อหรือรหัสผ่านไม่ถูกต้อง";
+    echo "<a href='loginD.php'>เข้าสู่ระบบอีกครัง</a>"; 
+  }
+?>
