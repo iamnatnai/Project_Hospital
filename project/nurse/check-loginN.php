4faa28f2444d314c996c607b3967a8913a4243fe
Phakin Neamson <phakin.neamson@gmail.com>
2022-11-01 10:47:40 +0700

    Update check-loginN.php

--- project/nurse/check-loginN.php
+++ project/nurse/check-loginN.php
@@ -12,18 +12,16 @@
   $stmt->execute();
   $row = $stmt->fetch();
 
-  // หาก username และ password ตรงกัน จะมีข้อมูลในตัวแปร $row
   if (!empty($row)) { 
     session_regenerate_id();
-    // นำข้อมูลผู้ใช้จากฐานข้อมูลเขียนลง session 2 ค่า
+
     $_SESSION["fullname"] = $row["nfnamelname"];   
     $_SESSION["username"] = $row["nid"];
 
-    // แสดง link เพื่อไปยังหน้าต่อไปหลังจากตรวจสอบสำเร็จแล้ว
     echo "เข้าสู่ระบบสำเร็จ<br>";
     header( "refresh:1; url=index.php" );
 
-  // กรณี username และ password ไม่ตรงกัน
+
   } else {
     echo "ไม่สำเร็จ ชื่อหรือรหัสผ่านไม่ถูกต้อง";
     header( "location:loginN.php?message=Username or Password not correct!" ); 
