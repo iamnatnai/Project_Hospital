602d4dfea59195a2bc28c5bceee623e4b9276b16
kaamncn <92039823+kaamncn@users.noreply.github.com>
2022-11-19 19:23:10 +0700

    a

--- project/doctor/ordersheet/insertdos.php
+++ project/doctor/ordersheet/insertdos.php
@@ -6,17 +6,19 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
 </head>
+
 <body>
-    <a href="insertdos.php">Insert</a> | 
-    <a href="editdos.php">Edit</a> | 
+    <a href="insertdos.php">Insert</a> |
+    <a href="editdos.php">Edit</a> |
     <a href="deletedos.php">Delete</a> <br><br>
-    <form action="insert.php" method="post">
-        <input type="hidden" name="did" value="<?=$_SESSION['usernameD']?>">
+    <form action="insertseeadoctor.php" method="post">
+        <input type="hidden" name="did" value="<?= $_SESSION['usernameD'] ?>">
         <label for="pid">Patient ID : </label>
-        <input type="text" name="pid"><br>
+        <input type="text" name="pid" id="pid"><br>
         <!-- <label for="id">Doctor's order sheet ID : </label>
         <input type="text" name="id" pattern="[O]\d{3}" title="">
         ** Use capital letter that start with O and folloed by 3 digits.
@@ -24,7 +26,8 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         <label for="guide">Guideline : </label><br>
         <textarea name="guide" cols="30" rows="5"></textarea>
         <br>-->
-        <input type="submit" value="OK"> 
+        <input type="submit" value="OK">
     </form>
 </body>
+
 </html>
---
+++ project/doctor/ordersheet/insertseeadoctor.php
@@ -0,0 +1,33 @@
+<?php
+    $pdo = new PDO("mysql:host=localhost;dbname=system_hospital;charset=utf8", "root", "");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    ?>
+    <?php
+    $status = "Active";
+    $seeadoctor = $pdo->prepare("insert into seeadoctor(did,pid,entrydate) values(?,?,curdate())");
+    $seeadoctor->bindParam(1,$_POST["did"]);
+    $seeadoctor->bindParam(2,$_POST["pid"]);
+    $seeadoctor->execute();
+
+// $stmt = $pdo->prepare("insert into dos (dosdate,dostime,guideline,status)
+//                     values(curdate(),curtime(),?,?)");
+// $stmt->bindParam(1, $_POST["id"]);
+// $stmt->bindParam(2, $_POST["guide"]);
+// $stmt->bindParam(3, $status);
+// $stmt->execute();
+// 
+?>
+
+<!-- <html>
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <a href="insertdos.php">Insert</a> |
+    <a href="editdos.php">Edit</a> |
+    <a href="deletedos.php">Delete</a> <br><br>
+</body>
+
+</html> -->
