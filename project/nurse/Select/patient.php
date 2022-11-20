65173fcd61997a60b6856620c0a8ced2049ab2fd
Phakin Neamson <phakin.neamson@gmail.com>
2022-11-20 19:45:54 +0700

    Miss You

--- project/nurse/Select/patient.php
+++ project/nurse/Select/patient.php
@@ -70,33 +70,33 @@ $patient->execute();
 <body>
     <?php include './nav.php' ?>
     <div>
-    <table>
-        <tr>
-            <th>Patient ID</th>
-            <th>Name</th>
-            <th>Date Of Birth</th>
-            <th>Age</th>
-            <th>Sex</th>
-            <th>To do</th>
-            <th>History</th>
-        </tr>
-        <?php while ($row = $patient->fetch()) : ?>
+        <table>
             <tr>
-                <td>
-                    <a href="patient_info.php?pid=<?= $row['pid'] ?>&sex=<?= $row['psex'] ?>"
-                        class="pid">
-                        <?= $row['pid'] ?>
-                    </a>
-                </td>
-                <td style="font-weight: bold;"><?= $row['pfnamelname'] ?></td>
-                <td><?= $row['pdob'] ?></td>
-                <td><?= $row['page'] ?></td>
-                <td><?= $row['psex'] ?></td>
-                <td><a href="./patient_follow.php?pid=<?= $row['pid'] ?>" class="pid">To do</a></td>
-                <td><a href="patient_history.php?pid=<?= $row['pid'] ?>" class="pid">History</a></td>
+                <th>Patient ID</th>
+                <th>Name</th>
+                <th>Date Of Birth</th>
+                <th>Age</th>
+                <th>Sex</th>
+                <th>To do</th>
+                <th>History</th>
             </tr>
-        <?php endwhile ?>
-    </table>
+            <?php while ($row = $patient->fetch()) : ?>
+                <tr>
+                    <td>
+                        <a href="patient_info.php?pid=<?= $row['pid'] ?>&sex=<?= $row['psex'] ?>"
+                            class="pid">
+                            <?= $row['pid'] ?>
+                        </a>
+                    </td>
+                    <td style="font-weight: bold;"><?= $row['pfnamelname'] ?></td>
+                    <td><?= $row['pdob'] ?></td>
+                    <td><?= $row['page'] ?></td>
+                    <td><?= $row['psex'] ?></td>
+                    <td><a href="./patient_follow.php?pid=<?= $row['pid'] ?>" class="pid">To do</a></td>
+                    <td><a href="patient_history.php?pid=<?= $row['pid'] ?>" class="pid">History</a></td>
+                </tr>
+            <?php endwhile ?>
+        </table>
     </div>
     <a href="./patient.php">Back</a>
 </body>
